e2f37bdb5bff14769df4bce25b6683c959d0ad4e
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-08-19 22:34:33 +0000

    make optionals, well... optional!

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 from admipaste import AdmiPaste, app
 import yaml
 
+with open("config.yml", "r") as f:
+    cfg = yaml.load(f.read())
+
+apaste = AdmiPaste(cfg["mysql"]["host"],
+                   cfg["mysql"]["user"],
+                   cfg["mysql"]["pass"],
+                   cfg["mysql"]["name"])
+
 
 def main():
-    with open("config.yml", "r") as f:
-        cfg = yaml.load(f.read())
-
-    apaste = AdmiPaste(cfg["mysql"]["host"],
-                    cfg["mysql"]["user"],
-                    cfg["mysql"]["pass"],
-                    cfg["mysql"]["name"])
     apaste.run(cfg["host"], cfg["port"], debug=bool(cfg["debug"]))
 
     return 0
